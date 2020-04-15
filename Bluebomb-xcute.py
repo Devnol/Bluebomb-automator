11aee4a1ccb0d794aa9c823170c95512a17efa43
CommandBlock6417 <58105634+CommandBlock6417@users.noreply.github.com>
2020-04-15 17:27:23 +0300

    Delete Bluebomb-xcute.py

--- Bluebomb-xcute.py
+++
@@ -1,7 +0,0 @@
-import os
-dir_comm = os.system("cd ~/Documents/git/Bluebomb-automator/")
-exec_comm = os.system("chmod +x ~/Documents/git/Bluebomb-automator/bluebomb-auto-helper.sh")
-run_comm = os.system("./bluebomb-auto-helper.sh -c 2 -r 2")
-print("directory changed with exit code %d" % dir_comm)
-print("program chmodded with exit code %d" % exec_comm)
-print("Bluebomb helper ran with exit code %d" % run_comm)
